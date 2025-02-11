d4b1a1e762c547874e30d08a4e5ddd30e67f6d62
Mirage <clementbobin21@gmail.com>
2025-02-11 15:26:41 +0000

    remove unused app.vue file, add Scalar module to nuxt.config.ts, and create API documentation in api.docs.yaml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  modules: ['@scalar/nuxt'],
+
+  nitro: {
+    experimental: {
+      openAPI: true,
+    },
+  },
+
+  // if you want to use the Scalar API Documentation UI (Statique, your own API documentation) else comment the scalar object
+  scalar: {
+    theme: "deepSpace",
+    darkMode: true,
+    hideModels: false,
+    hideDownloadButton: false,
+    metaData: {
+      title: 'API Documentation by Scalar',
+    },
+    proxyUrl: 'https://proxy.scalar.com',
+    searchHotKey: 'k',
+    showSidebar: true,
+    pathRouting: {
+      basePath: '/docs',
+    },
+    spec: {
+      url: 'http://127.0.0.1:3000/api.docs.yaml',
+    },
+  },
+
+  compatibilityDate: '2025-02-11',
 })
