3ccea5be5b8ef5f5271b0eb1627093326d0e95e4
Mirage <clementbobin21@gmail.com>
2025-02-12 08:23:55 +0000

    Remove unused weather summary array from Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -55,9 +55,4 @@ app.UseHttpsRedirection();
 
 app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.Run();
