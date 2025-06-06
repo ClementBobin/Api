d6cb76dba8bf9df876445ad8abbd771c302fe0ca
mirage <119869686+ClementBobin@users.noreply.github.com>
2025-06-06 21:33:36 +0200

    Add environment variable for enabling documentation and load dotenv configuration

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import { app } from './lib/express';
 import { apiReference } from '@scalar/express-api-reference';
 import { docs } from './lib/docs/docOpenApi';
+import dotenv from 'dotenv';
 
 // Importing routes
 import router from './modules/user/user.routes';
 
+dotenv.config(); // Load environment variables from .env file
+
 // Mount the API routes
 app.use('/api', router);
 
-// Swagger documentation route
-app.use(
-    '/docs',
-    apiReference({ spec: { content: docs } }),
-);
+if (process.env.DOC_ENABLE === 'true') {
+    // Swagger documentation route
+    app.use(
+        '/docs',
+        apiReference({ spec: { content: docs } }),
+    );
+}
