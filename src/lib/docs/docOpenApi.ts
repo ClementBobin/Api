02bb885d09d618c84bf02e99fd7f666bcfd47457
Mirage <119869686+ClementBobin@users.noreply.github.com>
2025-09-08 14:42:01 +0200

    Update OpenAPI document generation logic
    Refactor OpenAPI document generation to use config variables and remove dotenv.

--- src/lib/docs/docOpenApi.ts
+++ src/lib/docs/docOpenApi.ts
@@ -1,25 +1,14 @@
 // Import the OpenAPI registry from the local file
 import registry from './openAPIRegistry';
 
-import dotenv from 'dotenv'; // Importing dotenv to load environment variables from a .env file
+import { httpsPort, packageJson, productionDomain } from '../config/env.config';
 
 // Import the OpenApiGeneratorV3 class from the zod-to-openapi package
 import { OpenApiGeneratorV3 } from '@asteasolutions/zod-to-openapi';
 
-// Import the fs and path modules from Node.js
-import fs from 'fs';
-import path from 'path';
-
-// Load environment variables from the .env file
-dotenv.config();
 
 // Function to generate the OpenAPI document
 export const docs = () => {
-    // Read package.json to get the version
-    const packageJsonPath = path.resolve('.', 'package.json');
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-    const appVersion = packageJson.version;
-
     // Create a new instance of OpenApiGeneratorV3 with the registry definitions
     const generator = new OpenApiGeneratorV3(registry.definitions);
 
@@ -27,13 +16,16 @@ export const docs = () => {
     const openApiDocument = generator.generateDocument({
         openapi: '3.0.0', // OpenAPI version
         info: {
-            title: 'User API', // Title of the API
-            version: appVersion, // Version of the API
-            description: 'An API for managing user', // Description of the API
-        },
-        servers: [
-            { url: `http://localhost:${process.env.PORT}`, description: 'Development server' }, // Development server URL
-            { url: 'https://api.example.com', description: 'Production server' }, // Production server URL
+            title: packageJson.name, // Title of the API
+            version: packageJson.version, // Version of the API
+            // Description of the API
+                },
+                servers: [
+                    { 
+                        url: `https://localhost:${httpsPort}`, 
+                        description: 'Development server' 
+                    }, // Development server URL
+            { url: `http://${productionDomain}:${httpsPort}`, description: 'Production server' }, // Production server URL
         ],
     });
 
