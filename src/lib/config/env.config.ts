fe56eaa1de28ef8bc85958e1d60289602016d197
Mirage <119869686+ClementBobin@users.noreply.github.com>
2025-09-29 10:33:10 +0200

    Add rememberIpMinutes and numberRequestPerIp to exports

--- src/lib/config/env.config.ts
+++ src/lib/config/env.config.ts
@@ -16,5 +16,7 @@ const unixFormat = process.env.UNIX_FORMAT === 'true';
 const nodeEnv = process.env.NODE_ENV || 'development';
 const keepLogsFor = process.env.KEEP_LOGS_FOR || '90d';
 const docEnable = process.env.DOC_ENABLE === 'true';
+const rememberIpMinutes = parseInt(process.env.REMEMBER_IP_MINUTES || '5', 10);
+const numberRequestPerIp = parseInt(process.env.NUMBER_REQUEST_PER_IP || '80', 10);
 
-export { httpsPort, url, packageJson, productionUrl, logLevel, logFileEnabled, keepLogsInProd, logDirectory, dateFormat, storageDateFormat, unixFormat, nodeEnv, keepLogsFor, docEnable };
+export { httpsPort, url, packageJson, productionUrl, logLevel, logFileEnabled, keepLogsInProd, logDirectory, dateFormat, storageDateFormat, unixFormat, nodeEnv, keepLogsFor, docEnable, rememberIpMinutes, numberRequestPerIp };
