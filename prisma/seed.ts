d45c75fda680fbdfe4b0d8e85b6d9c69f341ed96
Mirage <119869686+ClementBobin@users.noreply.github.com>
2025-09-29 09:38:59 +0200

    Update seed.ts to enhance data seeding logic
    Refactored seeding process to include new data models and removed unused functions.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,33 @@
-import prisma from '../src/lib/db/prismaClient'; // Import the Prisma client
-import { generateMockUsers, generateMockProducts } from '../__mocks__/mockSchema';
+import prisma from '../src/lib/db/PrismaClient';
+import { closeServer } from '../src/lib/express';
+import {
+  capteurTypes,
+  sites,
+  capteurs,
+  siteHasCapteurs,
+  releverCapteurs,
+} from '../__mocks__/mockSchema';
 
-// Main function to orchestrate the seeding process
 async function main() {
-  await seedUsers();
-  await seedProducts();
-}
+  await prisma.capteurType.createMany({ data: capteurTypes });
 
-// Function to seed Users table
-async function seedUsers() {
-  const users = Array.from({ length: 10 }, generateMockUsers); // Generate 10 mock users
+  await prisma.site.createMany({ data: sites });
 
-  for (const user of users) {
-    await prisma.users.create({
-      data: user,
-    });
-  }
+  const capteurData = capteurs(capteurTypes);
+  await prisma.capteur.createMany({ data: capteurData });
 
-  console.log('Users seeded successfully');
-}
+  const siteHasCapteurData = siteHasCapteurs(capteurData, sites);
+  await prisma.siteHasCapteur.createMany({ data: siteHasCapteurData });
 
-// Function to seed Products table
-async function seedProducts() {
-  const users = await prisma.users.findMany({
-    select: { id: true },
-  });
-
-  const products = users.flatMap((user) =>
-    Array.from({ length: 5 }, () => generateMockProducts(user.id)) // Generate 5 products for each user
-  );
-
-  for (const product of products) {
-    await prisma.products.create({
-      data: product,
-    });
-  }
-
-  console.log('Products seeded successfully');
+  const releverCapteurData = releverCapteurs(capteurData);
+  await prisma.releverCapteur.createMany({ data: releverCapteurData });
 }
 
 // Execute the main function and handle errors
 main()
   .then(() => console.log('Seeding completed!')) // Log success message
   .catch((e) => console.error(e)) // Log any errors
-  .finally(() => prisma.$disconnect()); // Disconnect the Prisma client
+  .finally(() => {
+    prisma.$disconnect(); // Disconnect the Prisma client
+    closeServer(); // Close the server
+  });
